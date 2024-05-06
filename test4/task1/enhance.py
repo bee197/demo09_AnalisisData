8df3a94f57c01fba86c27b74c34c96d015f55782
bee197 <3483840638@qq.com>
2024-05-06 14:55:21 +0800

    命名pic 0-39

---
+++ test4/task1/enhance.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+# 读取图像
+image = cv2.imread('E:/20212333Python\demo09_AnalisisData/test4\pic\words3.jpg')
+
+# 对比度增强
+alpha = 1.5  # 对比度增强参数，可根据需要调整
+enhanced_image = cv2.convertScaleAbs(image, alpha=alpha, beta=0)
+
+# 亮度增强
+beta = 30  # 亮度增强参数，可根据需要调整
+enhanced_image = cv2.convertScaleAbs(image, alpha=1, beta=beta)
+
+# 锐化
+sharpening_filter = np.array([[-1, -1, -1],
+                              [-1, 9, -1],
+                              [-1, -1, -1]])  # 锐化滤波器
+sharpened_image = cv2.filter2D(enhanced_image, -1, sharpening_filter)
+
+# 显示原始图像和增强后的图像
+cv2.imshow('Original Image', image)
+cv2.imshow('Enhanced Image', enhanced_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('E:/20212333Python\demo09_AnalisisData/test4\pic/'+ "enhance" +'.png', enhanced_image)
